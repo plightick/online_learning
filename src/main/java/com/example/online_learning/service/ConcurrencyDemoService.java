b2213b5e7e36f3bedb9499685e837159319f47da
plightick <japanas2007@gmail.com>
2026-04-22 14:41:28 +0300

    ...

--- src/main/java/com/example/online_learning/service/ConcurrencyDemoService.java
+++ src/main/java/com/example/online_learning/service/ConcurrencyDemoService.java
@@ -8,7 +8,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.LockSupport;
 import org.springframework.stereotype.Service;
 
 @Service
